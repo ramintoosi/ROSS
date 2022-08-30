23373d83a8e043fe1c9f95afaf76f821a5b2706e
sammir95 <alimirsharji@gmail.com>
2022-08-30 09:46:46 +0430

    Adding Raw Data Default model

--- Python/ross_backend/resources/data.py
+++ Python/ross_backend/resources/data.py
@@ -94,3 +94,78 @@ class RawData(Resource):
             return {"message": "An error occurred inserting raw data."}, 500
 
         return "Success", 201
+        
+        
+class RawDataDefault(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('raw', type=str, required=True, help="This field cannot be left blank!")
+
+    @jwt_required
+    def get(self):      
+        user_id = get_jwt_identity() 
+        raw = RawModel.find_by_user_id(user_id)
+        if raw:
+            # b = io.BytesIO()
+            # b.write(raw.raw)
+            # b.seek(0)
+
+            # d = np.load(b, allow_pickle=True)
+            # print(d['raw'].shape) 
+            # b.close()
+            # print(raw.user_id, raw.project_id)
+            if raw.data:
+                response = flask.make_response(raw.data)
+                response.headers.set('Content-Type', 'application/octet-stream')
+                return response
+
+        return {'message': 'Raw Data does not exist'}, 404
+
+    @jwt_required
+    def post(self):
+        filestr = request.data  
+        user_id = get_jwt_identity()
+        if RawModel.find_by_user_id(user_id):
+            return {'message': "Raw Data already exists."}, 400
+
+        # data = RawData.parser.parse_args()
+
+        # print(eval(data['raw']).shape)
+        raw = RawModel(user_id = user_id, data = filestr)#data['raw'])
+
+        try:
+            raw.save_to_db()
+        except:
+            return {"message": "An error occurred inserting raw data."}, 500
+
+        return "Success", 201
+
+    @jwt_required
+    def delete(self):
+        user_id = get_jwt_identity()
+        raw = RawModel.find_by_user_id(user_id)
+        if raw:
+            raw.delete_from_db()
+            return {'message': 'Raw Data deleted.'}
+        return {'message': 'Raw Data does not exist.'}, 404
+
+    @jwt_required
+    def put(self): 
+        filestr = request.data  
+        user_id = get_jwt_identity() 
+        raw = RawModel.find_by_user_id(user_id)
+        if raw:
+            raw.data = filestr
+            try:
+                raw.save_to_db()
+            except:
+                return {"message": "An error occurred inserting raw data."}, 500
+            return "Success", 201
+        
+        else:
+            raw = RawModel(user_id, data=filestr)
+        try:
+            raw.save_to_db()
+        except:
+            return {"message": "An error occurred inserting raw data."}, 500
+
+        return "Success", 201
