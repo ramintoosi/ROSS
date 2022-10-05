08244070ff09fb76f0c3f3c026ae70e16caf7a43
sammir95 <alimirsharji@gmail.com>
2022-10-05 17:18:58 +0330

    spike detector function added

--- Python/ross_backend/resources/funcs/detection.py
+++ Python/ross_backend/resources/funcs/detection.py
@@ -78,3 +78,56 @@ def filtering(data, forder, fRp, fRs, sr):
     print('after filtfilt!')
     return data_filtered
 
+# Spike Detector
+def spike_detector(data, thr, pre_thresh, post_thresh, dead_time, side):
+    n_points_per_spike = pre_thresh + post_thresh + 1
+
+    # Thresholding
+    if side == "mean":
+        sign_mean = np.sign(np.mean(data))
+        if sign_mean > 0:
+            side = "positive"
+        else:
+            side = "negative"
+
+    if side == "both":
+        spike_detected = (data > thr) | (data < -thr)
+    elif side == "positive":
+        spike_detected = (data > thr)
+    elif side == "negative":
+        spike_detected = (data < -thr)
+    else:
+        raise Exception
+
+    ind_det = np.nonzero(spike_detected)[0]
+
+    for i, i_s in enumerate(ind_det):
+        if spike_detected[i_s] == 0:
+            continue
+        if i_s + n_points_per_spike - pre_thresh + dead_time <= len(data):
+            ind_min = np.argmin(data[i_s:i_s + n_points_per_spike - pre_thresh + dead_time])
+            spike_detected[i_s:i_s + n_points_per_spike - pre_thresh + dead_time] = 0
+            spike_detected[i_s + ind_min] = True
+        else:
+            ind_min = np.argmin(data[i_s:])
+            spike_detected[i_s:] = 0
+            spike_detected[i_s + ind_min] = True
+
+    # update detected indices by throwing away unwanted detections
+    indx_spikes = np.nonzero(spike_detected)[0]
+
+    SpikeMat = np.zeros((len(indx_spikes), n_points_per_spike))
+    SpikeTime = np.zeros((len(indx_spikes), 1))
+
+    # assigning SpikeMat and SpikeTime matrices
+    for i, curr_indx in enumerate(indx_spikes):
+        # check if all indices of the spike waveform are inside signal
+        if (((curr_indx - pre_thresh) > -1) and (curr_indx - pre_thresh + n_points_per_spike) <= len(data)):
+            SpikeMat[i, :] = data[curr_indx - pre_thresh: curr_indx - pre_thresh + n_points_per_spike]
+            SpikeTime[i] = curr_indx + 1
+
+    ind_rm = np.where(SpikeTime == 0)[0]
+    SpikeMat = np.delete(SpikeMat, ind_rm, axis=0)
+    SpikeTime = np.delete(SpikeTime, ind_rm, axis=0)
+
+    return SpikeMat, SpikeTime
