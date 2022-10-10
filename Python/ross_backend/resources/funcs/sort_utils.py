f2876693d6c8b39f5bf24f83dd7cf26ceadc5310
sammir95 <alimirsharji@gmail.com>
2022-10-10 14:23:04 +0330

    d_mixedmvST, matlab_round and statistical_filter added to sort_utils

--- Python/ross_backend/resources/funcs/sort_utils.py
+++ Python/ross_backend/resources/funcs/sort_utils.py
@@ -32,3 +32,43 @@ def dmvt_ls(y, mu, Sigma, landa, nu):
     t = scipy.stats.t(nu + p)
     dens = 2 * (denst) * t.cdf(np.sqrt((p + nu)/(mahalanobis_d + nu)) * np.expand_dims(np.sum(np.tile(np.expand_dims(np.linalg.lstsq(matrix_sqrt(Sigma).T, landa.T, rcond=None)[0], axis=0), (n, 1)) * (y - mu), axis=1), axis=1))
     return dens
+    
+
+def d_mixedmvST(y, pi1, mu, Sigma, landa, nu):
+    # y: the data matrix
+    # pi1: must be of the vector type of dimension g
+    # mu: must be of type list with g entries. Each entry in the list must be a vector of dimension p
+    # Sigma: must be of type list with g entries. Each entry in the list must be an matrix p x p
+    # lambda: must be of type list with g entries. Each entry in the list must be a vector of dimension p
+    # nu: a number
+    g = np.size(pi1)
+    dens = 0
+    for j in range(g):
+        dens = dens + pi1[j] * dmvt_ls(y, mu[j], Sigma[j], landa[j], nu)   # lnda
+    return dens
+
+
+def matlab_round(num):
+    res = (int(num > 0) - int(num < 0)) * int(abs(num) + 0.5)
+    return res
+
+
+# Statistical Filtering
+def statistical_filter(SpikeMat, config):
+    max_mean = config.max_mean
+    max_std = config.max_std
+    max_percent_outlier = config.max_outliers
+
+    spike_mean = np.mean(SpikeMat, axis=1)
+    spike_std = np.std(SpikeMat, axis=1)
+
+    L = (spike_std > max_std) | (spike_mean > max_mean) | (spike_mean < -max_mean)
+    ind = np.where(L == True)[0]
+    num_outliers = len(ind)
+
+    if num_outliers > max_percent_outlier * len(SpikeMat):
+        criterion = spike_std / np.std(spike_std) + np.abs(spike_mean) / np.std(spike_mean)
+        criterion_s = np.sort(criterion)[::-1]
+        L = criterion > criterion_s[np.ceil(len(L) * max_percent_outlier)]
+
+    return L
