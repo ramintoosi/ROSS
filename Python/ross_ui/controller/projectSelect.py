67bffd6c256bde45e11b5cba529199378f0ebac8
sammir95 <alimirsharji@gmail.com>
2022-11-06 16:10:02 +0330

    projectSelect added

---
+++ Python/ross_ui/controller/projectSelect.py
@@ -0,0 +1,7 @@
+from view.projectSelect import Project_Dialog
+
+class projectSelectApp(Project_Dialog):
+    def __init__(self, projects):
+        super(projectSelectApp, self).__init__(projects)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
