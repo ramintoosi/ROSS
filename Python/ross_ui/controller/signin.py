0cce69d263d1c22d0a380d57286ace87bea19efa
sammir95 <alimirsharji@gmail.com>
2022-11-20 19:24:54 +0330

    signin added

---
+++ Python/ross_ui/controller/signin.py
@@ -0,0 +1,33 @@
+from view.signin import Signin_Dialog
+from model.api import UserAccount
+from PyQt5 import QtWidgets
+
+class SigninApp(Signin_Dialog):
+    def __init__(self, server):
+        super(SigninApp, self).__init__(server)
+        self.pushButton_in.pressed.connect(self.accept_in)
+        self.pushButton_up.pressed.connect(self.accept_up)
+
+    def accept_in(self):
+        username = self.textEdit_username.text()
+        password = self.textEdit_password.text()
+        self.user = UserAccount(self.url)
+        res = self.user.sign_in(username, password)
+        if res['stat']:
+            super().accept()
+        else:
+            QtWidgets.QMessageBox.critical(self, "Error", res["message"])
+
+
+    def accept_up(self):
+        username = self.textEdit_username.text()
+        password = self.textEdit_password.text()
+        self.user = UserAccount(self.url)
+        res = self.user.sign_up(username, password)
+        if res['stat']:
+            self.label_res.setStyleSheet("color: green")
+            QtWidgets.QMessageBox.information(self, "Account Created", res["message"])
+        else:
+            self.label_res.setStyleSheet("color: red")
+        # self.label_res.setText(res['message'])
+            QtWidgets.QMessageBox.critical(self, "Error", res["message"])
