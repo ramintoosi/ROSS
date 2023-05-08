3a6c3d10125dfae4064c7478406cc2dcbed541bf
Ramin Toosi <ramin.tsi@gmail.com>
2023-05-08 19:24:56 +0330

    options - server dial modified

--- ross_ui/controller/matplot_figures.py
+++ ross_ui/controller/matplot_figures.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class MatPlotFigures:
     def __init__(self, fig_title, number_of_clusters, width=10, height=6, dpi=100):
         fig = plt.figure(figsize=(width, height), dpi=dpi)
-        fig.canvas.set_window_title(fig_title)
+        fig.canvas.manager.set_window_title(fig_title)
         self.axes = []
         nrows = math.ceil(number_of_clusters / 3)
         for i in range(number_of_clusters):
--- ross_ui/view/serverAddress.py
+++ ross_ui/view/serverAddress.py
@@ -4,18 +4,26 @@ from PyQt5 import QtCore, QtWidgets
 class Server_Dialog(QtWidgets.QDialog):
     def __init__(self, server_text):
         super().__init__()
-        self.setFixedSize(400, 227)
+        self.setFixedSize(400, 150)
         self.buttonBox = QtWidgets.QDialogButtonBox(self)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 160, 341, 32))
+        layout_main = QtWidgets.QVBoxLayout()
+        # self.buttonBox.setGeometry(QtCore.QRect(30, 160, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
 
         self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(QtCore.QRect(80, 50, 141, 21))
+        # self.label.setGeometry(QtCore.QRect(80, 50, 141, 21))
 
         self.lineEdit = QtWidgets.QLineEdit(self)
-        self.lineEdit.setGeometry(QtCore.QRect(80, 100, 221, 31))
+        # self.lineEdit.setGeometry(QtCore.QRect(80, 100, 221, 31))
 
         self.setWindowTitle("Server Address")
         self.label.setText("Enter Server Address:")
         self.lineEdit.setText(server_text)
+
+        layout_main.addWidget(self.label)
+        layout_main.addWidget(self.lineEdit)
+        layout_main.addWidget(QtWidgets.QLabel(""))
+        layout_main.addWidget(self.buttonBox)
+
+        self.setLayout(layout_main)
