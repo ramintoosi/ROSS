2f2bc3f70e04d38936360810fe179da6c008074a
Ramin Toosi <ramin.tsi@gmail.com>
2023-06-25 18:04:50 +0330

    gmm sorting - test

--- ross_backend/resources/funcs/gmm.py
+++ ross_backend/resources/funcs/gmm.py
@@ -3,27 +3,23 @@ from sklearn.metrics import silhouette_score
 from sklearn.mixture import GaussianMixture as GMM
 
 
-def gmm_sorter(alignedSpikeMat, ss):
-    out = dict()
-    print('GMM')
-
+def gmm_sorter(aligned_spikemat, ss):
     g_max = ss.g_max
     g_min = ss.g_min
     max_iter = ss.max_iter
     n_cluster_range = np.arange(g_min + 1, g_max + 1)
-    scores = []
     error = ss.error
 
+    scores = []
+
     for n_cluster in n_cluster_range:
-        clusterer = GMM(n_components=n_cluster, random_state=5, tol=error, max_iter=max_iter)
-        cluster_labels = clusterer.fit_predict(alignedSpikeMat)
-        silhouette_avg = silhouette_score(alignedSpikeMat, cluster_labels)
-        print('For n_cluster={0} ,score={1}'.format(n_cluster, silhouette_avg))
+        cluster = GMM(n_components=n_cluster, random_state=5, tol=error, max_iter=max_iter)
+        cluster_labels = cluster.fit_predict(aligned_spikemat)
+        silhouette_avg = silhouette_score(aligned_spikemat, cluster_labels)
         scores.append(silhouette_avg)
 
     k = n_cluster_range[np.argmax(scores)]
-    clusterer = GMM(n_components=k, random_state=5, tol=error, max_iter=max_iter)
-    out['cluster_index'] = clusterer.fit_predict(alignedSpikeMat)
-    print('clusters : ', out['cluster_index'])
+    cluster = GMM(n_components=k, random_state=5, tol=error, max_iter=max_iter)
+    cluster_index = cluster.fit_predict(aligned_spikemat)
 
-    return out['cluster_index']
+    return cluster_index
