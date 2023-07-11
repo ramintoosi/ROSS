77ff0b3fd96dc9ad37d6a1fa72c9ae4508d8ca54
Ramin Toosi <ramin.tsi@gmail.com>
2023-07-11 14:25:24 +0330

    Issue #7 - add port arg in server

--- ross_backend/app.py
+++ ross_backend/app.py
@@ -1,3 +1,5 @@
+import argparse
+
 import flask.scaffold
 
 from db import db
@@ -102,5 +104,11 @@ api.add_resource(Projects, '/projects')
 api.add_resource(Browse, '/browse')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--port", type=int, help="Server port number", default=5000)
+
+    args = parser.parse_args()
+
     db.init_app(app)
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=args.port, debug=False)
